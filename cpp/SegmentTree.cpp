6e81f152754162e824ed2230b2a2d42c0faede6b
n3 <alphagameplayerpro@gmail.com>
2026-07-01 13:06:33 +0900

    テンプレートの整理

--- cpp/SegmentTree.cpp
+++ cpp/SegmentTree.cpp
@@ -1,50 +1,96 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include <cmath>
-#include <set>
-#include <unordered_map>
-#include <map>
-#include <unordered_set>
-#include <queue>
-#include <algorithm>
-#include <iomanip>
-#include <functional>
-#include <cassert>
-
+#include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using LL = __int128;
+using ull = unsigned long long;
+using ld = long double;
 
-#define ll long long
-#define LL __int128
-#define MOD 998244353
-#define ld long double
-#define INF 2251799813685248
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-#define reps(i, l, r) for(ll i = (l); i < (r); ++i)
-#define vall(A) A.begin(),A.end()
-#define slice(A, l, r) next((A).begin(), (l)), next((A).begin(), (r))
-#define gridinput(vv,H,W) for (ll i = 0; i < H; i++){string T; cin >> T; for(ll j = 0; j < W; j++){vv[i][j] = {T[j]};}}
-#define adjustedgridinput(vv,H,W) for (ll i = 1; i <= H; i++){string T; cin >> T; for(ll j = 1; j <= W; j++){vv[i][j] = {T[j-1]};}}
-#define vin(A) for (ll i = 0, sz = A.size(); i < sz; i++){cin >> A[i];}
-#define vout(A) for(ll i = 0, sz = A.size(); i < sz; i++){cout << A[i] << " \n"[i == sz-1];}
-#define adjustedvin(A) for (ll i = 1, sz = A.size(); i < sz; i++){cin >> A[i];}
-#define adjustedvout(A) for(ll i = 1, sz = A.size(); i < sz; i++){cout << A[i] << " \n"[i == sz-1];}
-#define vout2d(A,H,W) for (ll i = 0; i < H; i++){for (ll j = 0; j < W; j++){cout << A[i][j] << " \n"[j==W-1];}}
-#define encode(i,j) ((i)<<32)+j
-#define decode(v,w) (w ? (v)%4294967296 : (v)>>32)
+/*repマクロ*/
+#define REP_CAT_INNER(a,b) a##b
+#define REP_CAT(a,b) REP_CAT_INNER(a,b)
+#define OVERLOAD_REP(a,b,c,d,name,...) name
+#define rep(...) OVERLOAD_REP(__VA_ARGS__,REP3,REP2,REP1,REP0)(__VA_ARGS__)
+#define REP0(x) for(auto REP_CAT(_rep_c_,__LINE__):std::views::iota(0LL,(long long)(x)))
+#define REP1(i,x) for(auto i:std::views::iota(0LL,(long long)(x)))
+#define REP2(i,l,r) for(auto i:std::views::iota((long long)(l),(long long)(r)))
+#define REP3(i,l,r,c) for(long long i=(long long)(l),REP_CAT(_rep_e_,__LINE__)=(long long)(r),REP_CAT(_rep_s_,__LINE__)=(long long)(c);(REP_CAT(_rep_s_,__LINE__)>0?i<REP_CAT(_rep_e_,__LINE__):i>REP_CAT(_rep_e_,__LINE__));i+=REP_CAT(_rep_s_,__LINE__))
+#define vall(A) (A).begin(),(A).end()
+#define lsegtype(name) name::S, name::F
+#define lsegarg(name) name::op, name::e,name::comp, name::mapping, name::id
 
-vector<ll> pow2ll{1,2,4,8,16,32,64,128,256,512,1024,2048,4096,8192,16384,32768,65536,131072,262144,524288,1048576,2097152,4194304,8388608,16777216,33554432,67108864,134217728,268435456,536870912,1073741824,2147483648,4294967296};
+vector<ll> pow2ll{1,2,4,8,16,32,64,128,256,512,1024,2048,4096,8192,16384,32768,65536,131072,262144,524288,1048576,2097152,4194304,8388608,16777216,33554432,67108864,134217728,268435456,536870912,1073741824,2147483648,4294967296,8589934592,17179869184,34359738368,68719476736,137438953472,274877906944,549755813888,1099511627776,2199023255552,4398046511104,8796093022208,17592186044416,35184372088832,70368744177664,140737488355328,281474976710656,562949953421312,1125899906842624,2251799813685248,4503599627370496,9007199254740992,18014398509481984,36028797018963968,72057594037927936,144115188075855872,288230376151711744,576460752303423488,1152921504606846976,2305843009213693952,4611686018427387904};
 vector<ll> pow10ll{1,10,100,1000,10000,100000,1000000,10000000,100000000,1000000000,10000000000,100000000000,1000000000000,10000000000000,100000000000000,1000000000000000,10000000000000000,100000000000000000,1000000000000000000};
-vector<ll> di{0,1,0,-1};
-vector<ll> dj{1,0,-1,0};
 
 template <typename T>
 bool chmax(T &a, const T& b) { return a < b ? a = b, true : false; }
 template <typename T>
 bool chmin(T &a, const T& b) { return a > b ? a = b, true : false; }
 
+template <typename T>
+T sum(vector<T> A){
+    T res = 0;
+    for (size_t i=0;i<A.size();i++){
+        res += A[i];
+    }
+    return res;
+}
 
-// ===========================================================================================================================
+ll powll(ll a, ll n, ll m){
+    if (n == 0){return 1;}
+    if (n == 1){return a % m;}
+    LL ans = 1;
+    LL p = a;
+    while(n > 0){
+        if ((n & 1) == 1){
+            ans *= p;
+            ans %= m;
+        }
+        n >>= 1;
+        p *= p;
+        p %= m;
+    }
+    return (ll)ans;
+}
+
+long long isqrt(long long n) {
+  if (n <= 0) return 0;
+  long long x = sqrt(n);
+  while ((x + 1) * (x + 1) <= n) x++;
+  while (x * x > n) x--;
+  return x;
+}
+
+template <typename T>
+inline int bit_length(T n) {
+    static_assert(std::is_integral_v<T>, "bit_length: unsupported type");
+    if (n <= 0) return 0; 
+    return std::bit_width(static_cast<std::make_unsigned_t<T>>(n));
+}
+
+template <typename T>
+inline int bit_count(T n) {
+    static_assert(std::is_integral_v<T>, "bit_count: unsupported type");
+    return std::popcount(static_cast<std::make_unsigned_t<T>>(n));
+}
+
+// {a,b} → {a^-1 mod b,gcd(a,b)}
+template <typename T>
+pair<T,T> exgcd(T a, T b){
+    T xs = 1, ys = 0, xt = 0, yt = 1, tmp;
+    while (b != 0){
+        tmp = a/b;
+        a = a%b;
+        xs -= tmp*xt;
+        ys -= tmp*yt;
+        swap(xs,xt);
+        swap(ys,yt);
+        swap(a,b);
+    }
+    return {xs,a};
+}
+// ===============================================================================
 
 // http://judge.u-aizu.ac.jp/onlinejudge/review.jsp?rid=11153823#1
 
@@ -154,11 +200,6 @@ struct SegmentTree{
     }
 };
 
-void print(vector<ll> A){
-    rep(i,A.size()){
-        cout << A[i] << " \n"[i == A.size()-1];
-    }
-}
 
 ll op(ll x,ll y){
   return x+y;
@@ -176,7 +217,7 @@ int main(){
 
 
     vector<ll> array(n);
-    vin(array);
+    // vin(array);
     
     auto ST = SegmentTree<ll>(array, op, e);
 
